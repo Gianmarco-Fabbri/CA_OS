0258bc8f83367ec96d63d330f61de88e9ce2bc8b
Gimbo <gianmarcofabbri2@gmail.com>
2025-10-09 19:24:43 +0200

    Updated work from the virtual machine

---
+++ Lessons/es/function.c
@@ -0,0 +1,8 @@
+#ifndef __FUNCTION__
+#define __FUNCTION__
+
+int add(const int a, const int b) {
+    return a + b;
+}
+
+#endif
---
+++ Lessons/es/main.c
@@ -0,0 +1,12 @@
+#include "function.h"
+
+#define N 5
+
+int main (int argc, char * argv[]) {
+
+    const int a = 2+N;
+    const int b = N-3;
+
+    int c = add(a, b);
+    return 0;
+}
